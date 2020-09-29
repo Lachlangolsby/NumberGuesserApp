1bf790f0023b20f31e43a5e100c9886c0a55aef9
Lachlangolsby <49334505+Lachlangolsby@users.noreply.github.com>
2020-09-29 19:33:42 +1000

    Working prototype with 3 activties and updated UI's. Added exception handling if invalid user input provided.

--- app/src/main/java/au/edu/unsw/infs3634/numberguesser/MainActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/numberguesser/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.InputMismatchException;
 import java.util.Random;
 
 public class MainActivity<difference> extends AppCompatActivity {
@@ -20,10 +21,13 @@ public class MainActivity<difference> extends AppCompatActivity {
 
         Random r = new Random();
         int low = 0;
-        int high = 100;
+        int high = 10;
         final int randomResult = r.nextInt(high - low) + low;
 
+
         final Button Button = (Button) findViewById(R.id.GuessButton);
+
+        final TextView tv3 = (TextView) findViewById(R.id.Error);
 
         Button.setOnClickListener(new View.OnClickListener() {
 
@@ -31,46 +35,58 @@ public class MainActivity<difference> extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-                i++;
-                assessGuess(randomResult, i);
-                attemptsRemaining(i, randomResult, Button);
-
+                EditText editText = findViewById(R.id.GuessField);
+                String temp = editText.getText().toString();
+                if (!temp.equals("")) {
+                    i++;
+                    assessGuess(randomResult,temp, i);
+                    attemptsRemaining(i, randomResult, Button);
+                    tv3.setText("");
+                }else {
+                    tv3.setText("Please enter a valid number");
+                }
             }
         });
 
     }
 
-    public void assessGuess(int randomNumber, int i) {
-
-        EditText editText = findViewById(R.id.GuessField);
-
-        String temp = editText.getText().toString();
-        int value = Integer.parseInt(temp);
-        int difference = value - randomNumber;
-
+    public void assessGuess(int randomNumber, String numberGuessed, int count) {
         TextView tv = (TextView) findViewById(R.id.highLowText);
         TextView tv2 = (TextView) findViewById(R.id.Attempts);
 
 
+
+
+
+        int value = Integer.parseInt(numberGuessed);
+        int difference = value - randomNumber;
+
+
+
+
         if (difference < 0) {
-            tv.setText("The number is higher than your previous guess !!");
-            tv2.setText("You Have " + (5 - i) + " Attempts Remaining");
+                tv.setText("The number is higher than your previous guess !!");
+                tv2.setText("You Have " + (5 - count) + " Attempts Remaining");
 
-        } else if (difference > 0) {
-            tv.setText("The number is Lower than your previous guess !!");
-            tv2.setText("You Have " + (5 - i) + " Attempts Remaining");
+            } else if (difference > 0) {
+                tv.setText("The number is Lower than your previous guess !!");
+                tv2.setText("You Have " + (5 - count) + " Attempts Remaining");
 
-        } else if (difference == 0) {
-            Intent activityChangeIntent = new Intent(MainActivity.this, CorrectGuess.class);
-            MainActivity.this.startActivity(activityChangeIntent);
-            Intent passNumber = new Intent(MainActivity.this, CorrectGuess.class);
-            passNumber.putExtra("randomNumber", randomNumber);
-            startActivity(passNumber);
+            } else if (difference == 0) {
+                Intent activityChangeIntent = new Intent(MainActivity.this, CorrectGuess.class);
+                MainActivity.this.startActivity(activityChangeIntent);
+                Intent passNumber = new Intent(MainActivity.this, CorrectGuess.class);
+                passNumber.putExtra("randomNumber", randomNumber);
+                startActivity(passNumber);
 
 
-        }
+
+            }
+
 
     }
+
+
 
     public void attemptsRemaining(int i, int randomNumber, Button Button) {
         EditText editText = findViewById(R.id.GuessField);
